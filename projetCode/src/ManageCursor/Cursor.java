9fdca203c11287bd78e4ca63c788227d26f719f5
Victand <163859192+Victand@users.noreply.github.com>
2024-05-15 14:24:16 +0200

    Update Cursor.java

--- projetCode/src/ManageCursor/Cursor.java
+++ projetCode/src/ManageCursor/Cursor.java
@@ -5,12 +5,12 @@ public class Cursor {
     int positionY;
     float angle;
     int id;
-    float thickness;
+    int thickness;
 
     boolean hidden;
-    Color color;
-
-    public Cursor(int positionX, int positionY, float angle, int id, float thickness, boolean hidden, Color color) {
+    Colorj color;
+    private Test testInstance;
+    public Cursor(int positionX, int positionY, float angle, int id, int thickness, boolean hidden, Colorj color) {
         this.positionX = positionX;
         this.positionY = positionY;
         this.angle = normalizeAngle(angle);
@@ -20,7 +20,22 @@ public class Cursor {
         this.color = color;
     }
 
-        private float normalizeAngle(float angle) {
+    public void mov(Position position) {
+    }
+
+    public void mov(Percentage x, Percentage y) {
+    }
+
+    public void position(Position position) {
+        this.positionX = position.getPositionX();
+        this.positionY = position.getPositionY();
+    }
+
+    //à determiner
+    public void position(Percentage x, Percentage y) {
+    }
+
+    private float normalizeAngle(float angle) {
         while (angle < 0) {
             angle += 360;
         }
@@ -29,59 +44,47 @@ public class Cursor {
         }
         return angle;
     }
-    public void mov(Position position){}
 
-    public void mov(Percentage x, Percentage y){}
+    public void forward(int value) {
+        double angleRadians = Math.toRadians(this.angle);
 
-    public void position(Position position){
-        this.positionX=position.getPositionX();
-        this.positionY=position.getPositionY();
+        double deltaX = value * Math.cos(-angleRadians);
+        double deltaY = value * Math.sin(-angleRadians);
+        this.positionX = (int) Math.round(this.positionX + deltaX);
+        this.positionY = (int) Math.round(this.positionY + deltaY);
+
+
     }
 
-    //à determiner
-    public void position(Percentage x, Percentage y){}
-
-    public void forward(int value){
-    if ((this.angle >=0) && (this.angle <=90)) {
-        this.positionX = (int) Math.round(this.positionX + (value*Math.cos(Math.toRadians(this.angle))));
-        this.positionY = (int) Math.round(this.positionY + (value*Math.sin(Math.toRadians(this.angle))));
+    public void forward(Percentage value) {
     }
-    else if ((this.angle >90) && (this.angle <=180)) {
-        this.positionX = (int) Math.round(this.positionX + (value*Math.cos(Math.toRadians(-this.angle))));
-        this.positionY = (int) Math.round(this.positionY + (value*Math.sin(Math.toRadians(this.angle))));
+
+    public void backward(int value) {
+        this.forward(-value);
     }
-    else if ((this.angle >180) && (this.angle <=270)) {
-        this.positionX = (int) Math.round(this.positionX + (value*Math.cos(Math.toRadians(-this.angle))));
-        this.positionY = (int) Math.round(this.positionY + (value*Math.sin(Math.toRadians(-this.angle))));
+
+    public void backward(Percentage value) {
     }
-    else if ((this.angle >270) && (this.angle <=359)) {
-        this.positionX = (int) Math.round(this.positionX + (value*Math.cos(Math.toRadians(this.angle))));
-        this.positionY = (int) Math.round(this.positionY + (value*Math.sin(Math.toRadians(-this.angle))));
+
+    public void turn(double angle) {
+        this.angle += angle;
+        normalizeAngle(this.angle);
     }
-    /*Il faut rajouter le fait que l'on dessine ici*/
-    /*surement un appelle de fonction de javafx pour mettre un trait dans une liste*/
-}
 
-    public void forward(Percentage value){}
+    public void press(Percentage value) {
+    }
 
-    public void backward(int value){}
+    public void thick(int value) {
+    }
 
-    public void backward(Percentage value){}
+    public void lookAt(int cursorID) {
+    }
 
-    public void turn(double angle){
-    this.angle+=angle;
-    normalizeAngle(this.angle);
-}
+    public void lookAt(Position position) {
+    }
 
-    public void press(Percentage value){}
-
-    public void thick(int value){}
-
-    public void lookAt(int cursorID){}
-
-    public void lookAt(Position position){}
-
-    public void lookAt(Percentage x, Percentage y){}
+    public void lookAt(Percentage x, Percentage y) {
+    }
 
     public int getPositionX() {
         return positionX;
@@ -115,11 +118,11 @@ public class Cursor {
         this.id = id;
     }
 
-    public float getThickness() {
-        return thickness;
+    public int getThickness() {
+        return this.thickness;
     }
 
-    public void setThickness(float thickness) {
+    public void setThickness(int thickness) {
         this.thickness = thickness;
     }
 
@@ -131,16 +134,20 @@ public class Cursor {
         this.hidden = hidden;
     }
 
-    public Color getColor() {
-        return color;
+    public int[] getColorj() {
+        return this.color.getRgb();
     }
 
-    public void setColor(Color color) {
+
+    public void setColor(Colorj color) {
         this.color = color;
     }
-        @Override
+
+    @Override
     public String toString() {
-        return String.format("Cursor[positionX=%d, positionY=%d, angle=%.2f, id=%d, thickness=%.2f, hidden=%b, color=%s]",
+        return String.format("Cursor[positionX=%d, positionY=%d, angle=%.2f, id=%d, thickness=%d, hidden=%b,"+this.color.toString()+"]",
                 positionX, positionY, angle, id, thickness, hidden, color);
     }
+
+}
 }
