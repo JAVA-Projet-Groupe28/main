53652fc90604f7f2f053d938d19f10d80a5fd742
YouennBogaer <163859975+YouennBogaer@users.noreply.github.com>
2024-05-23 13:22:17 +0200

    Update Colorj.java
    ajout des methodes setRgbFromWeb et setRgbFromDouble qui permettent d'avoir les valeur rgb (0à 255) a partir de l'hexa et du rgb en réels(de 0 à 1).

--- appProject/src/main/java/com/example/appproject/Colorj.java
+++ appProject/src/main/java/com/example/appproject/Colorj.java
@@ -3,20 +3,18 @@ package com.example.appproject;
 public class Colorj {
     int rgb[] = new int[3];
     String web;
-    double hsl[] = new double[3];
+    double rgb_double[] = new double[3];
 
     public Colorj(String web) {
         this.web = web;
+        setRgbFromWeb(web);
     }
 
-    public Colorj(double hue, double saturation, double luminosity) {
-        this.hsl[0] = hue;
-        this.hsl[1] = saturation;
-        this.hsl[2] = luminosity;
-
-        //calcul du code web
-
-
+    public Colorj(double red, double green, double blue) {
+        this.rgb_double[0] = red;
+        this.rgb_double[1] = green;
+        this.rgb_double[2] = blue;
+        setRgbFromRgbDouble(red,green,blue);
     }
 
     public Colorj(int red, int green, int blue)/* throws OutOfRangeRGBException*/ {
@@ -26,15 +24,44 @@ public class Colorj {
             this.rgb[2] = blue;
         }
         /*else throw new OutOfRangeRGBException("Couleurs non compris entre 0 et 255");*/
-
-        //calcul du code web
-        String redHex = Integer.toHexString(red);
-        String blueHex = Integer.toHexString(green);
-        String greenHex = Integer.toHexString(blue);
-
-        this.web = "#" + redHex + blueHex + greenHex;
     }
 
+
+    /**
+     * Set the rgb attribute based on the web description of the color in hexa.
+     * */
+    public void setRgbFromWeb(String webColor){
+        System.out.println("Color hexa :"+webColor);
+        int red = Integer.parseInt(webColor.substring(1,3),16);
+        int green = Integer.parseInt(webColor.substring(3,5),16);
+        int blue = Integer.parseInt(webColor.substring(5,7),16);
+        System.out.println("RGB : "+red +" "+ green +" "+ blue);
+        setRgb(red,green,blue);
+    }
+
+    /**
+     *Set the rgb value in integers from 0 to 255 of the color, from its rgb description in double value from 0 to 1.
+     */
+    public void setRgbFromRgbDouble(double red_d, double green_d, double blue_d){
+        int red = (int) Math.round(red_d*255);
+        int green = (int) Math.round(green_d*255);
+        int blue = (int) Math.round(blue_d*255);
+        setRgb(red,green,blue);
+    }
+
+    public void setRgb(int red, int green, int blue) {
+        this.rgb[0] = red;
+        this.rgb[1] = green;
+        this.rgb[2] = blue;
+    }
+
+    public int[] getRgb() {
+        return this.rgb;
+    }
+
+    /**
+     * Returns the web format of the color in hexa as a String, converted from the rgb value int[].
+     */
     public String rgbToWeb()/* throws OutOfRangeRGBException*/ {
         int red = this.rgb[0];
         int green = this.rgb[1];
@@ -53,31 +80,6 @@ public class Colorj {
         /*else throw new OutOfRangeRGBException("RGB format : color not included between 0 and 255");*/
         return this.web;
     }
-
-    public String getWeb() {
-        return this.web;
-    }
-
-    public void setWeb(String web) {
-        this.web = web;
-    }
-
-    public void setRgb(int red, int green, int blue) {
-        this.rgb[0] = red;
-        this.rgb[1] = green;
-        this.rgb[2] = blue;
-    }
-
-    public int[] getRgb() {
-        return this.rgb;
-    }
-
-    public void sethsl(double hue, double saturation, double luminosity) {
-        this.hsl[0] = hue;
-        this.hsl[1] = saturation;
-        this.hsl[2] = luminosity;
-    }
-
 
     @Override
     public String toString() {
